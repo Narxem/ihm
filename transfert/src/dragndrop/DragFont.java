a550ae39d37e06c5b5689e5710b86c8bc7114f55
Narxem <adrien.agez@hotmail.fr>
2016-03-15 12:12:00 +0100

    set transferhandler

--- transfert/src/dragndrop/DragFont.java
+++ transfert/src/dragndrop/DragFont.java
@@ -1,11 +1,15 @@
 package dragndrop;
 
+import java.awt.Font;
 import java.awt.LayoutManager;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 import javax.swing.BoxLayout;
 import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.TransferHandler;
 
 public class DragFont extends JFrame {
 	
@@ -17,11 +21,37 @@ public class DragFont extends JFrame {
 	
 	{
 		this.getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+		
+		// JLabel
 		this.add(this.label);
-		this.add(this.text1);
-		this.add(this.text2);
+		this.label.setTransferHandler(new TransferHandler("font"));
+		this.label.addMouseMotionListener(new MouseMotionListener() {
+			
+			@Override
+			public void mouseMoved(MouseEvent e) {
+				// Empty
+			}
+			
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				label.getTransferHandler().exportAsDrag(label, e, TransferHandler.COPY);
+				
+			}
+		});
+		
+		
+		// JFormattedTextField 1
 		text1.setDragEnabled(true);
+		text1.setFont(new Font("liberation", Font.BOLD | Font.ITALIC, 17));
+		this.add(this.text1);
+		
+		
+		// JFormattedTextField 2
 		text2.setDragEnabled(true);
+		this.add(this.text2);
+		
+		
+		
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setSize(300, 100);
 	}
