f20e47c7b726803491ef2344afee904d59c20165
Narxem <adrien.agez@hotmail.fr>
2016-04-25 22:49:47 +0200

    add MagneticGuides

---
+++ magnetic/src/magnetic/MagneticGuide.java
@@ -0,0 +1,9 @@
+package magnetic;
+
+import fr.lri.swingstates.canvas.CExtensionalTag;
+import fr.lri.swingstates.canvas.CSegment;
+
+public class MagneticGuide extends CExtensionalTag {
+
+	protected CSegment cs;
+}
